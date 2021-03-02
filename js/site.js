9cdc6a8df79c3f5b68cb6498ebda7a58370c6cd3
jacobrozio <jrozio@ucsc.edu>
2021-03-01 16:57:58 -0800

    adding genre pages

--- js/site.js
+++ js/site.js
@@ -1,11 +1,16 @@
 //take to genre page
 
-$("#get-ticket").onclick( function()
+
+// prints out an image of a ticket with user info once clicking button
+$("#get-ticket").click( function()
 {
   var nameVal = $("#ticket-name").val();
   var genreVal = $("#ticket-genre").val();
 
   $("#ticket-result").append("<div id='ticket-image' src='img/ticket.jpg'></div>");
-  $("#ticket-result").append("<p>Name: " + nameVal + "</p>");
-  $("#ticket-result").append("<p>Favorite Genre: " + genreVal + "</p>");
+  $("#ticket-result").append("<div><p>Name: " + nameVal + "</p>" +
+  "<p>Favorite Genre: " + genreVal + "</p>");
 })
+
+//
+$("#enter").click()
